d67db6aa7b59fd894e827eec5478ea9c3d2ce774
Gautam Kishore <gautam@eulogik.com>
2026-05-20 13:08:53 +0530

    feat: update google tag manager id to GTM-WWT6Z9RN

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TT27KG5Z');`,
+})(window,document,'script','dataLayer','GTM-WWT6Z9RN');`,
           }}
         />
       </head>
       <body className="min-h-full">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TT27KG5Z"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WWT6Z9RN"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
